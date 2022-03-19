95f0d5a31a62e3adcceffa76866d2ce5432e4b87
Sudhanshu_Jain <jainsudhanshu10@gmail.com>
2022-03-19 16:17:13 +0530

    Add files via upload

---
+++ SimpleMathsOperations/MathOperations/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class MathOperations
+    {
+
+        public static double Add(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+
+        public static double Subtract(double number1, double number2)
+        {
+            return (number1 - number2);
+        }
+
+        public static double Multiply(double number1, double number2)
+        {
+            return (number1 * number2);
+        }
+
+        public static double Divide(double number1, double number2)
+        {
+            return (number1 / number2);
+        }
+
+    }
+}
